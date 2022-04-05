6c6599a6a9d02913c102126dcd75508829c5a8cd
ccor <ccor@itu.dk>
2022-04-05 10:50:24 +0200

    something.

---
+++ code/model_run_hardcases.py
@@ -0,0 +1,212 @@
+import csv
+import re
+import numpy as np
+import pandas as pd
+import torch
+from transformers import BertTokenizer
+import json
+import torch.nn as nn
+from transformers import BertModel
+from datetime import datetime
+import torch
+import random
+
+import pickle
+
+TEST  = '../data/group12.json'
+
+now = datetime.now()
+current_time = now.strftime("_%d%m%Y_%H%M%S")
+batch_size = 2
+
+## functions
+def loader(PATH):
+    with open(PATH, mode ='r')as file:
+        csvFile = csv.reader(file)
+        text = []
+        for lines in csvFile:
+            text.append(lines)
+    return text
+
+if torch.cuda.is_available():       
+    device = torch.device("cuda")
+    #print(f'There are {torch.cuda.device_count()} GPU(s) available.')
+    #print('Device name:', torch.cuda.get_device_name(0))
+else:
+    #print('No GPU available, using the CPU instead.')
+    device = torch.device("cpu")
+
+def set_seed(seed_value=42):
+    """Set seed for reproducibility.
+    """
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)
+
+# load data
+X_test = loader(TEST) 
+X_test = X_test[0:100]
+
+# Fix X_test so that it is a single list of strings
+test = []
+for i in range(len(X_test)):
+    test.append(str(X_test[i][0]))
+X_test = test
+
+# tokenise with BERT
+from transformers import BertTokenizer
+
+# Load the BERT tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+
+# Create a function to tokenize a set of texts
+def preprocessing_for_bert(data):
+    """Perform required preprocessing steps for pretrained BERT.
+    @param    data (np.array): Array of texts to be processed.
+    @return   input_ids (torch.Tensor): Tensor of token ids to be fed to a model.
+    @return   attention_masks (torch.Tensor): Tensor of indices specifying which
+                  tokens should be attended to by the model.
+    """
+    # Create empty lists to store outputs
+    input_ids = []
+    attention_masks = []
+
+    # For every sentence...
+    for sent in data:
+        # `encode_plus` will:
+        #    (1) Tokenize the sentence
+        #    (2) Add the `[CLS]` and `[SEP]` token to the start and end
+        #    (3) Truncate/Pad sentence to max length
+        #    (4) Map tokens to their IDs
+        #    (5) Create attention mask
+        #    (6) Return a dictionary of outputs
+        encoded_sent = tokenizer.encode_plus(
+            text=sent,  # Preprocess sentence
+            add_special_tokens=True,        # Add `[CLS]` and `[SEP]`
+            max_length=MAX_LEN,                  # Max length to truncate/pad
+            padding='max_length',         # Pad sentence to max length
+            #return_tensors='pt',           # Return PyTorch tensor
+            return_attention_mask=True,      # Return attention mask
+            truncation = True
+            )
+        
+        # Add the outputs to the lists
+        input_ids.append(encoded_sent.get('input_ids'))
+        attention_masks.append(encoded_sent.get('attention_mask'))
+
+    # Convert lists to tensors
+    input_ids = torch.tensor(input_ids)
+    attention_masks = torch.tensor(attention_masks)
+
+    return input_ids, attention_masks
+
+MAX_LEN = 512
+
+# BERT model defs
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+
+# Specify loss function
+loss_fn = nn.CrossEntropyLoss()
+
+set_seed(42)    # Set seed for reproducibility
+
+class BertClassifier(nn.Module):
+    """Bert Model for Classification Tasks.
+    """
+    def __init__(self, freeze_bert=False):
+        """
+        @param    bert: a BertModel object
+        @param    classifier: a torch.nn.Module classifier
+        @param    freeze_bert (bool): Set `False` to fine-tune the BERT model
+        """
+        super(BertClassifier, self).__init__()
+        # Specify hidden size of BERT, hidden size of our classifier, and number of labels
+        D_in, H, D_out = 768, 50, 2
+
+        # Instantiate BERT model
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+
+        # Instantiate an one-layer feed-forward classifier
+        self.classifier = nn.Sequential(
+            nn.Linear(D_in, H),
+            nn.ReLU(),
+            #nn.Dropout(0.5),
+            nn.Linear(H, D_out)
+        )
+
+        # Freeze the BERT model
+        if freeze_bert:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+        
+    def forward(self, input_ids, attention_mask):
+        """
+        Feed input to BERT and the classifier to compute logits.
+        @param    input_ids (torch.Tensor): an input tensor with shape (batch_size, max_length)
+        @param    attention_mask (torch.Tensor): a tensor that hold attention mask
+                      information with shape (batch_size, max_length)
+        @return   logits (torch.Tensor): an output tensor with shape (batch_size, num_labels)
+        """
+        # Feed input to BERT
+        outputs = self.bert(input_ids=input_ids,
+                            attention_mask=attention_mask)
+        
+        # Extract the last hidden state of the token `[CLS]` for classification task
+        last_hidden_state_cls = outputs[0][:, 0, :]
+
+        # Feed input to classifier to compute logits
+        logits = self.classifier(last_hidden_state_cls)
+
+        return logits
+
+#############################################################
+bert_classifier = pickle.load(open('model_ALL_ALL.pkl', 'rb'))
+print('model loaded')
+
+
+# inference
+# Run `preprocessing_for_bert` on the test set
+test_inputs, test_masks = preprocessing_for_bert(X_test)
+
+# Create the DataLoader for our test set
+test_dataset = TensorDataset(test_inputs, test_masks)
+test_sampler = SequentialSampler(test_dataset)
+test_dataloader = DataLoader(test_dataset, sampler=test_sampler, batch_size=batch_size)
+
+import torch.nn.functional as F
+
+def bert_predict(model, test_dataloader):
+    """Perform a forward pass on the trained BERT model to predict probabilities
+    on the test set.
+    """
+    # Put the model into the evaluation mode. The dropout layers are disabled during
+    # the test time.
+    model.eval()
+
+    all_logits = []
+
+    # For each batch in our test set...
+    for batch in test_dataloader:
+        # Load batch to GPU
+        b_input_ids, b_attn_mask = tuple(t.to(device) for t in batch)[:2]
+
+        # Compute logits
+        with torch.no_grad():
+            logits = model(b_input_ids, b_attn_mask)
+        all_logits.append(logits)
+    
+    # Concatenate logits from each batch
+    all_logits = torch.cat(all_logits, dim=0)
+
+    # Apply softmax to calculate probabilities
+    probs = F.softmax(all_logits, dim=1).cpu().numpy()
+
+    return probs
+
+# Compute predicted probabilities on the test set
+probs = bert_predict(bert_classifier, test_dataloader)
+
+with open ('../data/probabilities/12hardprobs.csv', 'w') as f:
+    for i in probs:
+        f.writelines(str(i)+',')
---
+++ code/predict_max_hardcases.py
@@ -0,0 +1,57 @@
+import numpy as np
+import json
+import pandas as pd
+from datetime import datetime
+
+now = datetime.now()
+current_time = now.strftime("%d%m_%H%M")
+
+def get_predictions(N, PATH):
+    # Get predictions from the probabilities
+    threshold = N
+    probs = []
+    with open('../data/probabilities/' + PATH, 'r') as f:
+        temp = (f.read().split(','))
+        for i in temp[:-1]:
+            #print('i', i)
+            a = i.split(' ')
+            #print(len(a))
+            t = []
+            for u in a:
+                if len(u) > 1:
+
+                    t.append(float(u[1:-1]))
+            #print((t))
+            probs.append(t)
+    #print(probs)
+    probs = np.array(probs)
+    #print(probs)
+    preds = np.argmax(probs, axis=1)
+    #print(preds)
+
+    # Number of tweets predicted non-negative
+    #print("Number of reviews predicted positive: ", preds.sum())
+
+    # convert 0s and 1s into strings
+    y_hat = []
+
+    for i in preds:
+        if i:
+            y_hat.append('positive')
+        else: y_hat.append('negative')
+    # for export 
+    test_pred = pd.read_json( '../data/group12,json', lines=True)
+    #test_pred = test_pred.iloc[0:100]
+    test_pred['sentiment'] = y_hat
+    #Write in a way that codalabs will accept. Thanks to Nicola.
+    new = test_pred.to_dict('records')
+    test_json=[json.dumps(i)+'\n' for i in new]
+    with open ('../data/predictions/hardcases_predictions'+'.json', 'w') as file:
+        file.writelines(test_json)
+
+
+if __name__ == '__main__':
+    import sys
+    args = sys.argv
+    #print(args)
+    get_predictions(args[1])
