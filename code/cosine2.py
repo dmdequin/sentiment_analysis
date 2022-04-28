58e1b3db9514b9a72e62f08604bfe4787b0abc21
Danielle <62339718+dmdequin@users.noreply.github.com>
2022-04-28 16:55:40 +0200

    working on cosine similarity

--- code/cosine2.py
+++ code/cosine2.py
@@ -8,6 +8,8 @@ import heapq
 
 import gensim
 from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 #####################################################
 # Functions
@@ -22,35 +24,29 @@ if len(args) < 2:
 FILE_1 = args[1]  # name of interim csv file. For example: # games_train
 FILE_2 = args[2]  # name of comparison interim csv file. For example: # sew_train
 N_DIS = int(args[3])   # number of dissimilar embeddings to select
-# python3 cosine.py 'music_dev' 'sew_val' 2
+# python3 cosine.py 'music_dev' 'music_dev' 2
 
 # Load Interim CSV file and split into X and y
 data_1 = csv_loader('../data/interim/' + FILE_1 + '.csv')
 X_1, y_1 = data_1[['review']], data_1[['sentiment']]
 
-# Tokenize each review and lowercase everything
+# make the corpus a list of strings
 corp_1 = []
-for i in range(1,2): 
+for i in range(5): 
     row = X_1.iloc[i]['review']
-    tokenized = [w.lower() for w in word_tokenize(row)]
-    corp_1.append(tokenized)
+    corp_1.append(row)
 
 print(f"First Corpus: {corp_1[0]}")
 
-# dictionary of tokens
-dictionary = gensim.corpora.Dictionary(corp_1)
+# Use TFIDF to lower impact of frequent words
+count_vectorizer = TfidfVectorizer(stop_words='english')
+sparse_matrix = count_vectorizer.fit_transform(corp_1)
 
-# make BOW
-corpus = [dictionary.doc2bow(gen_doc) for gen_doc in corp_1]
-
-# TFIDF to downplay frequent words
-tf_idf = gensim.models.TfidfModel(corpus)
-#for doc in tf_idf[corpus]:
-#    print([[dictionary[id], np.around(freq, decimals=2)] for id, freq in doc])
-
-# building the index
-sims = gensim.similarities.Similarity('workdir/',tf_idf[corpus],
-                                        num_features=len(dictionary))
+# OPTIONAL: Convert Sparse Matrix to Pandas Dataframe if you want to see the word frequencies.
+doc_term_matrix = sparse_matrix.todense()
+df = pd.DataFrame(doc_term_matrix, 
+                  columns=count_vectorizer.get_feature_names_out())
+#print(df.head)
 
 #############################################################
 # Load Second Interim CSV file and split into X and y
@@ -58,32 +54,25 @@ data_2 = csv_loader('../data/interim/' + FILE_2 + '.csv')
 X_2, y_2 = data_2[['review']], data_2[['sentiment']]
 
 corp_2 = []
-avg_sims = [] # array of averages
-for i in range(1,2): 
+for i in range(5): 
     row = X_2.iloc[i]['review']
-    tokenized = [w.lower() for w in word_tokenize(row)] # Tokenize each review and lowercase everything
-    query_doc_bow = dictionary.doc2bow(tokenized) # update an existing dictionary and create bag of words
-    corp_2.append(tokenized)
-    
-    # perform a similarity query against the corpus
-    query_doc_tf_idf = tf_idf[query_doc_bow]
-    
-    doc_sim = sims[query_doc_tf_idf]
-    # print(document_number, document_similarity)
-    #print('Comparing Result:', doc_sim)
-    
-    sum_of_sims =(np.sum(doc_sim, dtype=np.float32))
-    sim_ave = round(sum_of_sims/len(corp_1), 2)
-    avg_sims.append((sim_ave,i))
-    
-    #print(f"Average Similarity: {sim_ave}")
-    
+    corp_2.append(row)
+
+#########################################
+# compute cosine similarity
+co_sim = cosine_similarity(df, df)
+
+avg_sims = [] # array of averages
+for i in range(len(corp_2): 
+    avg_sims.append(("thing",i))    
 
 print(f"\nSecond corpus: {corp_2[0]}")
 
 print("\nNumber of documents to compare:",len(corp_2))  
 print(f"\nAverage similarities: {avg_sims}")
 
+########################################
+# Find most dissimilar
 pq = heapq.nsmallest(N_DIS, avg_sims, key=None)
 print(f"\nPriority Q: {pq})")
 
